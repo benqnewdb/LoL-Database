6379fd5c87c295cd470ec55f72fe54d831eddb12
benqdb <92702958+benqdb@users.noreply.github.com>
2021-11-01 16:40:14 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -164,8 +164,7 @@ function showChamp(champName) {
   var script = scriptData[champ.systemName] || []
   var str =
     script.length == 0 ?
-    '<div class="center noFound"><span class="flow-text nonFound grey-text text-darken-2">No scripts found for ' +
-    champ.name +
+    '<div class="center noFound"><span class="flow-text nonFound grey-text text-darken-2">champ.name + 공략이 존재하지 
     '</span><div>' :
     ''
   for (i = 0; i < script.length; i++) {
